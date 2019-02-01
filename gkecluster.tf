bf642b5f3346022853a935884d3156bb12814653
Faizal Yusuf <faizal@gobear.com>
2019-02-01 11:25:17 +0800

    changed cluster name

--- gkecluster.tf
+++ gkecluster.tf
@@ -1,5 +1,5 @@
 resource "google_container_cluster" "gke-cluster" {
-  name       = "my-gke-cluster"
+  name       = "faizal-gke-cluster"
   network    = "default"
   zone       = "asia-southeast1-a"
   initial_node_count = 1
