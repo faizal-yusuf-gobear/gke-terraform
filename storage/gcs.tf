409fecec31d587818405855dc7f0201ad9549633
Faizal Yusuf <faizal@gobear.com>
2019-02-01 16:18:33 +0800

    removed backend

--- storage/gcs.tf
+++ storage/gcs.tf
@@ -3,7 +3,7 @@ resource "google_storage_bucket" "gke-tf-state" {
   location = "asia-southeast1-a"
 }
 
-resource "google_storage_bucket_acl" "image-store-acl" {
+resource "google_storage_bucket_acl" "tf-store-acl" {
   bucket = "${google_storage_bucket.gke-tf-state.name}"
   predefined_acl = "authenticatedRead"
 }
