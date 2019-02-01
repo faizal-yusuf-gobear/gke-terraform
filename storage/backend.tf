c83366879ae5fb968f26e1d206b6c51beb445e05
Faizal Yusuf <faizal@gobear.com>
2019-02-01 16:16:22 +0800

    removed backend

--- storage/backend.tf
+++
@@ -1,9 +0,0 @@
-resource "google_storage_bucket" "gke-tf-state" {
-  name     = "gke-terraform-state"
-  location = "asia-southeast1-a"
-}
-
-resource "google_storage_bucket_acl" "state-store-acl" {
-  bucket = "${google_storage_bucket.gke-tf-state.name}"
-  predefined_acl = "authenticatedRead"
-}
