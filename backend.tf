157243d04e9dddb1897a81546a600d1cd59547ae
Faizal Yusuf <faizal@gobear.com>
2019-02-01 14:55:45 +0800

    changed backedng

--- backend.tf
+++ backend.tf
@@ -1,7 +1,6 @@
 terraform {
   backend "gcs" {
     bucket  = "gke-terraform-backend"
-    credentials = "creds/serviceaccount.json"
     prefix  = "terraform/state"
   }
 }
--- provider.tf
+++ provider.tf
@@ -1,5 +1,4 @@
 provider "google" {
-  credentials = "${file("./creds/serviceaccount.json")}"
   project     = "my-gke-project"
   region      = "asia-southeast1-a"
 }
