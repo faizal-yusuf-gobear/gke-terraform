0eaeb5aca7a38d03ed5806ae9c7b2d7dbf375b25
Faizal Yusuf <faizal@gobear.com>
2019-02-06 20:12:44 +0800

    :-)

--- gkecluster.tf
+++ gkecluster.tf
@@ -1,5 +1,5 @@
 resource "google_container_cluster" "gke-cluster" {
-  name       = "faizal-gke-cluster"
+  name       = "gke-app-1"
   network    = "default"
   zone       = "asia-southeast1-a"
   initial_node_count = 1
