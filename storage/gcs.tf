2078c02261a9285855c89c5c3f78b7dfd0420916
Faizal Yusuf <faizal@gobear.com>
2019-02-06 17:54:24 +0800

    modified gcs

--- storage/gcs.tf
+++ storage/gcs.tf
@@ -7,7 +7,6 @@ resource "google_storage_bucket_acl" "tf-store-acl" {
   bucket = "${google_storage_bucket.gke-tf-state.name}"
 
   role_entity = [
-    "OWNER:user-my.email@gmail.com",
-    "READER:group-mygroup",
+    "OWNER:faizalace84@gmail.com",
   ]
 }
