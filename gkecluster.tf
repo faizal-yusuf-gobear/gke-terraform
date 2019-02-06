2261fbff1e7053d0fe9200d670b50a2841a083b5
Faizal Yusuf <faizal@gobear.com>
2019-02-06 21:08:24 +0800

    :-)

--- gkecluster.tf
+++ gkecluster.tf
@@ -2,5 +2,30 @@ resource "google_container_cluster" "gke-cluster" {
   name       = "gke-app-1"
   network    = "default"
   zone       = "asia-southeast1-a"
-  initial_node_count = 1
+  remove_default_node_pool = true
+
+  node_pool {
+    name = "default-pool"
+  }
+}
+
+resource "google_container_node_pool" "gke-cluster" {
+  name       = "primary-pool"
+  cluster    = "${google_container_cluster.primary.name}"
+  zone       = "asia-southeast1-a"
+  node_count = "1"
+
+  node_config {
+    machine_type = "n1-standard-1"
+  }
+  
+  autoscaling {
+    min_node_count = 1
+    max_node_count = 3
+  }
+  
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
 }
---
+++ provide.tf
@@ -0,0 +1,4 @@
+provider "google" {
+  project     = "gke-app-230805"
+  region      = "asia-southeast1-a"
+}
