ce0b1dbee268333cefe68cf4078e6f860e0bf86f
Faizal Yusuf <faizal@gobear.com>
2019-02-07 17:45:54 +0800

    :-)

--- main.tf
+++ main.tf
@@ -70,7 +70,7 @@ resource "google_container_cluster" "k8sexample" {
 #}
 
 resource "google_sql_database_instance" "master" {
-  name = "master-sqldb"
+  name = "master-instance"
   database_version = "MYSQL_5_6"
   # First-generation instance regions are not the conventional
   # Google Compute Engine regions. See argument reference below.
