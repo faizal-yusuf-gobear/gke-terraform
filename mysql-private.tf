364f709dca4394a2ee3a666a043d07c56ec1614b
Faizal Yusuf <faizal@gobear.com>
2019-02-07 14:57:22 +0800

    added mysql

---
+++ mysql-private.tf
@@ -0,0 +1,12 @@
+resource "google_sql_database_instance" "master" {
+  name = "master-instance"
+  database_version = "MYSQL_5_6"
+  # First-generation instance regions are not the conventional
+  # Google Compute Engine regions. See argument reference below.
+  region = "asia-southeast1-a"
+
+  settings {
+    tier = "D0"
+  }
+}
+
